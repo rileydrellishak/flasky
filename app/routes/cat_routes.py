8af807b096a099c13579146b7f66599068adf64e
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-19 22:56:10 -0400

    Updates cat routes with refactoring, docstring for validate_cat_id

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -17,19 +17,27 @@ def get_all_cats():
 
 @cats_bp.get('/<id>')
 def get_cat_by_id(id):
+    cat = validate_cat_id(id)
+    return dict(
+        id=cat.id,
+        name=cat.name,
+        color=cat.color,
+        personality=cat.personality
+    )
+
+def validate_cat_id(id):
+    '''
+    Checks if a cat with a given id exists. If id is incorrect type, returns 400 bad request error. If id does not exist in cats (list), returns 404 not found. If cat id found, returns the instance of cat.
+    '''
     try:
         cat_id = int(id)
     except:
-        return {'message': f'cat {id} invalid'}, 400
+        response = {'message': f'cat {id} invalid'}
+        abort(make_response(response, 400))
+    
     for cat in cats:
         if cat.id == cat_id:
-            return dict(
-            id=cat.id,
-            name=cat.name,
-            color=cat.color,
-            personality=cat.personality
-        )
-    return {'message': f'cat {cat_id} does not exist'}, 404
+            return cat
 
-def validate_cat_id(id):
-
+    response = {'message': f'cat {cat_id} does not exist'}
+    abort(make_response(response, 404))
