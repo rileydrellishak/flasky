8bd2499bb48841d11093d52b2b2aff9cdb237c02
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-25 17:55:40 -0400

    Completes models set up for cat

--- app/models/cat.py
+++ app/models/cat.py
@@ -1,13 +1,8 @@
-class Cat:
-    def __init__(self, id, name, color, personality):
-        self.id = id
-        self.name = name
-        self.color = color
-        self.personality = personality
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-cats = [
-    Cat(1, 'Bixby', 'brown', 'sleepy'),
-    Cat(2, 'Maille', 'orange', 'happy'),
-    Cat(3, 'Miles', 'gray', 'grumpy'),
-    Cat(4, 'Buddy', 'white', 'hungry')
-]
+class Cat(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name = Mapped[str]
+    color = Mapped[str]
+    personality = Mapped[str]
--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,43 +1,22 @@
 from flask import Blueprint, abort, make_response
-from app.models.cat import cats
 
 cats_bp = Blueprint('cats_bp', __name__, url_prefix='/cats')
 
-@cats_bp.get('')
-def get_all_cats():
-    result = []
-    for cat in cats:
-        result.append(dict(
-            id=cat.id,
-            name=cat.name,
-            color=cat.color,
-            personality=cat.personality
-        ))
-    return result
 
-@cats_bp.get('/<id>')
-def get_cat_by_id(id):
-    cat = validate_cat_id(id)
-    return dict(
-        id=cat.id,
-        name=cat.name,
-        color=cat.color,
-        personality=cat.personality
-    )
 
-def validate_cat_id(id):
-    '''
-    Checks if a cat with a given id exists. If id is incorrect type, returns 400 bad request error. If id does not exist in cats (list), returns 404 not found. If cat id found, returns the instance of cat.
-    '''
-    try:
-        cat_id = int(id)
-    except:
-        response = {'message': f'cat {id} invalid'}
-        abort(make_response(response, 400))
+# def validate_cat_id(id):
+#     '''
+#     Checks if a cat with a given id exists. If id is incorrect type, returns 400 bad request error. If id does not exist in cats (list), returns 404 not found. If cat id found, returns the instance of cat.
+#     '''
+#     try:
+#         cat_id = int(id)
+#     except:
+#         response = {'message': f'cat {id} invalid'}
+#         abort(make_response(response, 400))
     
-    for cat in cats:
-        if cat.id == cat_id:
-            return cat
+#     for cat in cats:
+#         if cat.id == cat_id:
+#             return cat
 
-    response = {'message': f'cat {cat_id} does not exist'}
-    abort(make_response(response, 404))
+#     response = {'message': f'cat {cat_id} does not exist'}
+#     abort(make_response(response, 404))
