3852ba703c88eb15cf5089c889f61fb757df8e89
Riley Drellishak <rileydrellishak@gmail.com>
2025-12-18 15:31:37 -0500

    update app to include cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .db import db, migrate
 from app.routes.cat_routes import cats_bp
 from app.routes.dog_routes import dogs_bp
@@ -7,6 +8,7 @@ import os
 def create_app(config=None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+    CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
